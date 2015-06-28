7ebad518deb2df6f7083d5467b3a6f725fb8af8a
Molly Kent <kent.molly@gmail.com>
2015-06-28 12:21:15 -0700

    trying to build a carousel

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,22 @@
 'use strict';
 $(function() {
+  var caroCounter = 2;
 
+  $('#carousel').on('click', 'button#caro-right', function() {
+    console.log($(this));
+  });
+
+  $('#carousel').on('click', 'button#caro-left', function() {
+    console.log($(this));
+  });
+
+  $('#carousel').on('click', 'div.caro-item', function() {
+
+    var popped = $(this).html;
+    console.log(popped);
+
+  });
   
 
 });
+

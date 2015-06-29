3a1da6ccceb6e41da7486d76a3955092f6b7fcf4
Molly Kent <kent.molly@gmail.com>
2015-06-29 10:32:16 -0700

    made clickable rotator for featured topics

--- public/js/home.js
+++ public/js/home.js
@@ -1,19 +1,55 @@
 'use strict';
 $(function() {
-  var caroCounter = 2;
+  var toPop;
 
   $('#carousel').on('click', 'button#caro-right', function() {
-    console.log($(this));
+    toPop = $(this).siblings('.diva');
+    if ($(toPop).attr('id') === 'caro-1') {
+      $(toPop).next().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else if ($(toPop).attr('id') === 'caro-2') {
+      $(toPop).next().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else if ($(toPop).attr('id') === 'caro-3') {
+      $(toPop).next().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else if ($(toPop).attr('id') === 'caro-4') {
+      $(toPop).next().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else if ($(toPop).attr('id') === 'caro-5') {
+      $(toPop).next().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else {
+      $(this).siblings('#caro-1').removeClass('backup').addClass('diva');
+      $(this).siblings('#caro-6').removeClass('diva').addClass('backup');
+    }
+    
   });
 
   $('#carousel').on('click', 'button#caro-left', function() {
-    console.log($(this));
+    toPop = $(this).siblings('.diva');
+    if ($(toPop).attr('id') === 'caro-6') {
+      $(toPop).prev().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else if ($(toPop).attr('id') === 'caro-5') {
+      $(toPop).prev().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else if ($(toPop).attr('id') === 'caro-4') {
+      $(toPop).prev().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else if ($(toPop).attr('id') === 'caro-3') {
+      $(toPop).prev().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else if ($(toPop).attr('id') === 'caro-2') {
+      $(toPop).prev().removeClass('backup').addClass('diva');
+      $(toPop).removeClass('diva').addClass('backup');
+    } else {
+      $(this).siblings('#caro-6').removeClass('backup').addClass('diva');
+      $(this).siblings('#caro-1').removeClass('diva').addClass('backup');
+    }
   });
 
   $('#carousel').on('click', 'div.caro-item', function() {
-
-    var popped = $(this).html;
-    console.log(popped);
 
   });
   
